ca3df907664f2e626c287ce50b8e1638a47c5bbb
illlama <beatlemaniakih@gmail.com>
2020-02-20 16:55:04 +0900

    Movie 요청

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,10 @@
 import fetch from 'node-fetch';
 
-const API_URL = 'https://yts.mx/api/v2/list_movies.json?';
+const MOVIES_LIST_URL = 'https://yts.mx/api/v2/list_movies.json?';
+const MOVIE_DETAIL_URL = 'https://yts.mx/api/v2/movie_details.json?';
 
 export const getMovies = (limit, rating) => {
-  let REQUEST_URL = API_URL;
+  let REQUEST_URL = MOVIES_LIST_URL;
   if (limit > 0) {
     REQUEST_URL += `limit=${limit}`;
   }
@@ -13,4 +14,15 @@ export const getMovies = (limit, rating) => {
   return fetch(REQUEST_URL)
     .then(res => res.json())
     .then(json => json.data.movies);
+};
+
+export const getMovie = id => {
+  let REQUEST_URL = MOVIE_DETAIL_URL;
+  if (id > 0) {
+    REQUEST_URL += `movie_id=${id}`;
+  }
+  console.log(REQUEST_URL);
+  return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movie);
 };
