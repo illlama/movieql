7228b825e9a18f0bda39107e2d0c4f629952de34
illlama <beatlemaniakih@gmail.com>
2020-02-20 17:17:26 +0900

    getSuggestions 까지. 끝

--- graphql/db.js
+++ graphql/db.js
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch';
-
-const MOVIES_LIST_URL = 'https://yts.mx/api/v2/list_movies.json?';
-const MOVIE_DETAIL_URL = 'https://yts.mx/api/v2/movie_details.json?';
+const BASE_URL = 'https://yts.mx/api/v2/';
+const MOVIES_LIST_URL = `${BASE_URL}list_movies.json?`;
+const MOVIE_DETAIL_URL = `${BASE_URL}movie_details.json?`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json?`;
 
 export const getMovies = (limit, rating) => {
   let REQUEST_URL = MOVIES_LIST_URL;
@@ -17,12 +18,16 @@ export const getMovies = (limit, rating) => {
 };
 
 export const getMovie = id => {
-  let REQUEST_URL = MOVIE_DETAIL_URL;
-  if (id > 0) {
-    REQUEST_URL += `movie_id=${id}`;
-  }
-  console.log(REQUEST_URL);
+  let REQUEST_URL = `${MOVIE_DETAIL_URL}movie_id=${id}`;
+
   return fetch(REQUEST_URL)
     .then(res => res.json())
     .then(json => json.data.movie);
+};
+
+export const getSuggestions = id => {
+  let REQUEST_URL = `${MOVIE_SUGGESTIONS_URL}movie_id=${id}`;
+  return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 };
